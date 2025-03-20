1f93df0539cb250fd50392fadf8f87075d5a2f96
Amarilio de Oliveira <amarilioalencar@gmail.com>
2025-03-20 12:00:48 +0000

    fix: wrap useSearchParams() in a Suspense boundary to prevent error on /404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,37 +7,35 @@ import Footer from "@/components/layout/Footer";
 
 const NotFound: React.FC = () => {
   return (
-    <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <section className="hero bg-base-200 min-h-screen">
-          <div className="hero-content text-center">
-            <div className="max-w-xl text-left">
-              <span className="flex gap-2">
-                <h1 className="mb-6 text-5xl font-bold bg-gradient-to-r from-accent to-error inline-block text-transparent bg-clip-text">
-                  Oops! Error 404
-                </h1>
-                <Icon
-                  icon="line-md:alert-circle-loop"
-                  className="size-6 text-error"
-                />
-              </span>
+    <Suspense fallback={<div>Loading...</div>}>
+      <section className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-xl text-left">
+            <span className="flex gap-2">
+              <h1 className="mb-6 text-5xl font-bold bg-gradient-to-r from-accent to-error inline-block text-transparent bg-clip-text">
+                Oops! Error 404
+              </h1>
+              <Icon
+                icon="line-md:alert-circle-loop"
+                className="size-6 text-error"
+              />
+            </span>
 
-              <p className="font-light mb-8">
-                It seems the page you&apos;re looking for doesn&apos;t exist or
-                has been moved.
-              </p>
+            <p className="font-light mb-8">
+              It seems the page you&apos;re looking for doesn&apos;t exist or
+              has been moved.
+            </p>
 
-              <Link href="/" className="btn justify-between">
-                <span>Go back to home</span>
-                <Icon icon="line-md:arrow-small-right" className="size-5" />
-              </Link>
-            </div>
+            <Link href="/" className="btn justify-between">
+              <span>Go back to home</span>
+              <Icon icon="line-md:arrow-small-right" className="size-5" />
+            </Link>
           </div>
-        </section>
-      </Suspense>
+        </div>
+      </section>
 
       <Footer />
-    </>
+    </Suspense>
   );
 };
 
